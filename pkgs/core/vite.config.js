2cd4eb2a6b3a3ffa0dccb007fcb0a167d22de4ff
Kevin Bridges <kevin@wiiBridges.com>
2023-10-09 12:53:02 -0500

    refine realistic "core" library bundle using vite (now npm client dynamically selects appropriate bundle)

--- pkgs/core/vite.config.js
+++ pkgs/core/vite.config.js
@@ -1,15 +1,16 @@
 export default {
   build: {
     outDir:  'dist',      // the bundle output directory ... defaults to: dist
-    outFile: 'vzual.js',  // the bundled JS files, generating vzual.js (UMD format) -and- vzual.mjs (ES module format) ... defaults to: lib-name.js
     lib: {
       name:  'vzual',        // our library name
       entry: 'src/index.js', // the bundle entry point
+      formats: ['es',        // ES6 Bundle:      dist/vzual.mjs      (import)
+                'cjs',       // CommonJS Bundle: dist/vzual.cjs      (require)
+                'umd'],      // UMD Bundle:      dist/vzual.umd.js   (import)
     },
     rollupOptions: {
       external: ['socket.io'], // do NOT include external dependencies in our bundle (let consumer know they must install this dependency themselves
       output: {
-        format: 'umd', // UMD format, supporting BOTH ES6 (import), and CommonJS (require)
         globals: {
           socketIO: 'Socket.IO', // Map external dependencies to global variables if needed
         },
